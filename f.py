c485d1c48dcb05a2ce16a6161556e4d7523e650a
rohanp2814 <rohanp2814@gmail.com>
2025-08-16 19:48:35 +0530

    initial

---
+++ f.py
@@ -0,0 +1,33 @@
+from pyrogram import Client, filters
+from threading import Thread
+from flask import Flask
+
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+SESSION_NAME = os.getenv("SESSION_NAME", "my_session")
+SOURCE_CHANNELS = os.getenv("SOURCE_CHANNELS").split(",")
+DEST_CHANNEL = os.getenv("DEST_CHANNEL")
+
+
+
+
+app = Client(SESSION_NAME, api_id=API_ID, api_hash=API_HASH)
+flask_app = Flask(__name__)
+
+@flask_app.route("/")
+def home():
+    return "Bot is alive!"
+
+@app.on_message(filters.chat(SOURCE_CHANNELS))
+async def forward_to_channel(client, message):
+    await message.forward(DEST_CHANNEL)
+
+def run_flask():
+    flask_app.run(host="0.0.0.0", port=8080)
+
+if __name__ == "__main__":
+    # Run Flask in a separate thread
+    Thread(target=run_flask).start()
+    # Run Pyrogram client
+    app.run()
